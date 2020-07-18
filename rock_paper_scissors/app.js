8fa6b66bd81c946402e396a3a541883156b309b7
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-07-18 09:49:07 -0400

    js game is working button is not working

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -1,17 +1,139 @@
-const choiceButtons = document.querySelectorAll('[data-selection]');
+let rock = document.getElementById('Rock');
+let paper = document.getElementById('Paper');
+let scissors = document.getElementById('Scissors');
 
+let userChoice = prompt("Do you choose rock, paper or scissors?");
 
-choiceButtons.forEach(choiceButtons => {
-    choiceButtons.addEventListener('click', e => {
-        const choice = choiceButtons.dataset.selection;
-        makeChoice(choice);
-    })
-})
+let computerChoice = Math.ceil(Math.random()*3);
+  if (computerChoice === 1) {
+	computerChoice = "rock";
+  } else if(computerChoice === 2) {
+	computerChoice = "paper";
+  } else {
+	computerChoice = "scissors";
+  } 
+  //console.log("Computer chooses: " + computerChoice);
 
-function makeChoice(selection) {
-    console.log(selection);
+let compare = function(choice1,choice2) {
+    
+    if (choice1 === choice2) {
+        
+        return "The result is a tie!" + " " + "Lets play again." ;
+    }
+    
+    else if (choice1 === "rock") {
+        
+        if (choice2 === "scissors") {
+            return "rock wins" + "<br>" + "You beat the computer, nice job!";
+        }
+        else {
+            return "paper wins" + "<br>" + "Your really smart computer beat you.";
+        }
+    }
+    
+    else if (choice1 === "paper") {
+        
+        if (choice2 === "rock") {
+            return "paper wins" + "<br>" + "You beat the computer, nice job!";
+        }
+        else {
+            return "scissors wins" + "<br>" + "Your really smart computer beat you.";
+        }
+            
+    }
+    
+    else if (choice1 === "scissors") {
+        
+        if (choice2 === "rock") {
+            return "rock wins" + "<br>" + "Your really smart computer beat you.";
+        }
+        else {
+            return "scissors win" + "<br>" + "You beat the computer, nice job!";
+        }
+    }
+    
+    
+    } //closes compare function
+
+    
+    
+   document.write("Computer chose: " + computerChoice + "<br>");
+   document.write(compare(userChoice,computerChoice));
+
+/*
+   function game() {
+    let userChoice = prompt("Do you choose rock, paper or scissors?");
+
+    let computerChoice = Math.ceil(Math.random()*3);
+        if (computerChoice === 1) {
+	    computerChoice = "rock";
+        } else if(computerChoice === 2) {
+	    computerChoice = "paper";
+        } else {
+	    computerChoice = "scissors";
+        } 
+
+    let compare = function(choice1,choice2) {
+    
+        if (choice1 === choice2) {
+        
+        return "The result is a tie!" + " " + "Lets play again." ;
+    }
+    
+    else if (choice1 === "rock") {
+        
+        if (choice2 === "scissors") {
+            return "rock wins" + "<br>" + "You beat the computer, nice job!";
+        }
+        else {
+            return "paper wins" + "<br>" + "Your really smart computer beat you.";
+        }
+    }
+    
+    else if (choice1 === "paper") {
+        
+        if (choice2 === "rock") {
+            return "paper wins" + "<br>" + "You beat the computer, nice job!";
+        }
+        else {
+            return "scissors wins" + "<br>" + "Your really smart computer beat you.";
+        }
+            
+    }
+    
+    else if (choice1 === "scissors") {
+        
+        if (choice2 === "rock") {
+            return "rock wins" + "<br>" + "Your really smart computer beat you.";
+        }
+        else {
+            return "scissors win" + "<br>" + "You beat the computer, nice job!";
+        }
+    }
+    
+    
+    } //closes compare function
+
+    
+   document.write("Computer chose: " + computerChoice + "<br>");
+   document.write(compare(userChoice,computerChoice));
+
 }
+  /* 
 
+/*
+function randomComputerChoice() {
+    let computerChoice = Math.ceil(Math.random()*3);
+        if (computerChoice === 1) {
+	    computerChoice = "rock";
+        } else if(computerChoice === 2) {
+	    computerChoice = "paper";
+        } else {
+	    computerChoice = "scissors";
+        } 
+    return computerChoice;
+}
+*/
 
 
 
