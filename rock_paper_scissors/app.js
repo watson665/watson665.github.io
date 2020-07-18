fe813f72b662686ccd57e4faa22fa60747e81ee9
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-07-17 23:27:02 -0400

    new game with 3 buttons (for rock, paper, & scissors per Jose's suggestion). Almost done

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -1,55 +1,15 @@
-let user = document.querySelector('.userchoice');
-let computer = document.querySelector('.computerchoice');
-let startButton = document.querySelector('.start-button');
-let result = document.querySelector('.result');
-let userChoice;
-//object shows which options win over the other options
-let toWin = {
-    Rock: 'Scissors', 
-    Paper: 'Rock', 
-    Scissors: 'Paper'}
-//Add score variables
-let userScore;
-let computerScore;
-
-//Start function start the game
-    //What will happen after user clicks 'play'
-startButton.addEventListener('click', function(){
-    //disable start button 
-    startButton.disabled = true;
-    //no results
-    result.innerHTML= '';
-    //changes class name in HTML
-    user.className = 'userchoice Rock countToThree';
-    computer.className = 'computerchoice Rock countToThree';
-});
+const choiceButtons = document.querySelectorAll('[data-selection]');
 
 
-//When countToThree ends this will occur next
-document.addEventListener('animationend', function(){
-    startButton.disabled = false;
-    let computerChoice = getRandomChoice();
-    let userChoice = getRandomChoice();
-    computer.className = `computerChoice ${computerChoice}`;
-    user.className = `userChoice ${userChoice}`;
-    result.innerHTML = determineWinner(userChoice, computerChoice);
+choiceButtons.forEach(choiceButtons => {
+    choiceButtons.addEventListener('click', e => {
+        const choice = choiceButtons.dataset.selection;
+        makeChoice(choice);
+    })
 })
 
-function getRandomChoice() {
-    return Object.keys(toWin)[Math.floor(Math.random() * 3)];
-
-}
-
-function determineWinner(userChoice, computerChoice) {
-    if(userChoice === computerChoice) {
-        return `It's a draw!`;
-    } else if (userChoice === toWin[computerChoice]) {
-        //add 1 to pc score
-        //computerScore += 1;
-        return `Computer Wins! ${computerChoice} beats ${userChoice}.`;
-    } 
-   //userScore += 1;
-    return `You Win! ${userChoice} beats ${computerChoice}.`;
+function makeChoice(selection) {
+    console.log(selection);
 }
 
 
